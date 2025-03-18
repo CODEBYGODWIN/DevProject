7334f480ff9bd77469a916e033fa8095f634fa0b
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-03-18 07:16:07 +0100

    user

--- Match-App/src/Form/UserEditType.php
+++ Match-App/src/Form/UserEditType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Document\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,59 +13,17 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\Regex;
-use Symfony\Component\Validator\Constraints\LessThanOrEqual;
-use App\Document\User;
 
 class UserEditType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'constraints' => [
-                    new NotBlank(['message' => "L'email est requis"]),
-                    new Email(['message' => "L'email n'est pas valide"]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => "L'email doit avoir au moins 8 caractères",
-                    ]),
-                ],
-            ])
-            ->add('password', PasswordType::class, [
-                'constraints' => [
-                    new NotBlank(['message' => "Le mot de passe est requis"]),
-                    new Length([
-                        'min' => 8,
-                        'minMessage' => "Le mot de passe doit contenir au moins 8 caractères",
-                    ]),
-                    new Regex([
-                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
-                        'message' => "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial",
-                    ]),
-                ],
-            ])
-            ->add('username', TextType::class, [
-                'constraints' => [
-                    new NotBlank(['message' => "Le nom d'utilisateur est requis"]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => "Le nom d'utilisateur doit avoir au moins 6 caractères",
-                    ]),
-                ],
-            ])
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('username', TextType::class)
             ->add('birthdate', DateType::class, [
                 'widget' => 'single_text',
-                'constraints' => [
-                    new NotBlank(['message' => "La date de naissance est requise"]),
-                    new LessThanOrEqual([
-                        'value' => '-18 years',
-                        'message' => "Vous devez avoir au moins 18 ans pour vous inscrire",
-                    ]),
-                ],
             ])
             ->add('gender', ChoiceType::class, [
                 'choices' => [
@@ -73,11 +32,13 @@ class UserEditType extends AbstractType
                     'Autre' => 'other',
                 ],
             ])
+          
             ->add('bio', TextType::class, ['required' => false])
             ->add('picture', FileType::class, [
                 'required' => false,
-                'data_class' => null,
+                'data_class' => null, 
             ])
+            
             ->add('save', SubmitType::class, ['label' => 'Créer un compte']);
     }
 
